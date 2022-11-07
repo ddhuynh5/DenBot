e9dac9ac7c43954908e810cc4e38373e1e05a988
Dennis <ddhuynh@clemson.edu>
2022-11-06 22:27:31 -0500

    removed kick event (fix later), fixed on vc join event

--- bot.py
+++ bot.py
@@ -1,15 +1,15 @@
 # Imports
 import os
+import discord
 from tkinter.messagebox import askyesno
 from unittest import async_case
-import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-client = commands.Bot(command_prefix='!')
+client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 
 # on startup
 
@@ -27,28 +27,16 @@ async def on_ready():
 @client.event
 async def on_voice_state_update(member, before, after):
     if not before.channel and after.channel and member.id == 210225328402989056:
-        andy_id = "217419100018704384"
+        andy_id = 217419100018704384
         channel = client.get_channel(395879704328142849)
         await channel.send(f"<@{andy_id}> get on :smiling_imp:")
 
+
 # code to react to user msg
-
-
 @client.event
 async def on_message(message):
     if message.content == "pog":
         await message.channel.send('very pog')
 
-# code to kick a user on first vc join instance
-
-
-@client.event
-async def on_voice_state_update(member, before, after):
-    kick_counter = 0
-    while (kick_counter == 0):
-        if not before.channel and after.channel and member.id == 181438247015022592:
-            channel = client.get_channel(395879704328142849)
-            await member.move_to(None)
-            kick_counter = 1
 
 client.run(TOKEN)
