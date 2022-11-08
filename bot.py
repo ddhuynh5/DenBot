579557e172feafff4890c4b0b69568521ca731a5
Dennis <ddhuynh@clemson.edu>
2022-11-07 21:51:57 -0500

    adding link check, hopefully fixing attachment check

--- bot.py
+++ bot.py
@@ -37,7 +37,7 @@ async def on_message(message):
         elif message.content == "very pog":
             await message.channel.send('the poggest')
 
-    if len(message.attachments) > 0 and member.id == 181438247015022592:
+    if (message.attachments or 'https://' in message) and member.id == 181438247015022592:
         await message.channel.send('fuk of')
         await message.delete()
 
