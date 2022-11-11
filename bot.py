8b3c345e4c37ccd5ebb3e5dcf08fdd7c96280514
Dennis <ddhuynh@clemson.edu>
2022-11-11 00:00:01 -0500

    testing intents
    forgot to add 'removed link req.txt'

--- bot.py
+++ bot.py
@@ -6,8 +6,9 @@ import requests
 from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
-
-client = commands.Bot(command_prefix="$", intents=discord.Intents.default())
+intents = discord.Intents.default()
+intents.message_content = True
+client = commands.Bot(command_prefix="$", intents=intents)
 
 # on startup
 
