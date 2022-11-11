de6409a5b479df522819b6798f808c206cdd2283
Dennis <ddhuynh@clemson.edu>
2022-11-11 00:21:17 -0500

    test

--- bot.py
+++ bot.py
@@ -14,4 +14,20 @@ async def test(ctx):
     await ctx.send("test")
 
 
+@client.command(name="users", description="shows number of members in server")
+async def users(ctx):
+    await ctx.send(f"""This server has {id.member_count} members""")
+
+
+@client.command()
+async def cat(ctx, arg):
+    await ctx.channel.send(arg)
+    #url = "https://api.thecatapi.com/v1/images/search"
+    #response = requests.get(url)
+    #res = response.json()
+    
+    #for r in res:
+    #    await ctx.send(r["url"])
+
+
 client.run(TOKEN)
