901a3ba9547dbe5b95d8448d51470969049247e0
Dennis <ddhuynh@clemson.edu>
2022-11-11 00:16:55 -0500

    test without code

--- bot.py
+++ bot.py
@@ -7,57 +7,11 @@ from discord.ext import commands
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 
-client = commands.Bot(command_prefix="$$$", intents=discord.Intents.all())
-
-# on startup
-
-
-@client.event
-async def on_ready():
-    print("Connected to bot: {}".format(client.user.name))
-    print("Bot ID: {}".format(client.user.id))
-
-# code to mention a user whenever another user joins a voice channel
-# in this example, my friend andy will be pinged
-# andy_id = 217419100018704384
-
-
-@client.event
-async def on_voice_state_update(member, before, after):
-    if not before.channel and after.channel and member.id == 210225328402989056:
-        andy_id = 217419100018704384
-        channel = client.get_channel(395879704328142849)
-        await channel.send(f"<@{andy_id}> get on :smiling_imp:")
-
-
-# code to react to user msg
-@client.event
-async def on_message(message):
-    if not message.author.bot:
-        if message.content == "pog":
-            await message.channel.send("very pog")
-        elif message.content == "very pog":
-            await message.channel.send("the poggest")
-
-    if len(message.attachments) > 0 and message.author.id == 181438247015022592:
-        await message.channel.send("Nice try dumbass")
-        await message.delete()
-
-
-@client.command(name="users", description="shows number of members in server")
-async def users(ctx):
-    await ctx.send(f"""This server has {id.member_count} members""")
-
+client = commands.Bot(command_prefix="!")
 
 @client.command()
-async def cat(ctx, arg):
-    await ctx.channel.send(arg)
-    #url = "https://api.thecatapi.com/v1/images/search"
-    #response = requests.get(url)
-    #res = response.json()
-    
-    #for r in res:
-    #    await ctx.send(r["url"])
+async def test(ctx):
+    await ctx.send("test")
 
 
 client.run(TOKEN)
