83b4b359a583350bf2da3eb29ddd0384cb4095c9
Dennis <ddhuynh@clemson.edu>
2023-01-04 18:41:27 -0500

    removed local testing imp

--- bot.py
+++ bot.py
@@ -6,9 +6,9 @@ import random
 import discord
 
 from discord.ext import commands
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
