ce8ee4f6ccb433dbfbfc6ce111d4cf43ca164711
Dennis <ddhuynh@clemson.edu>
2023-01-06 21:35:40 -0500

    removing dotenv req

--- bot.py
+++ bot.py
@@ -5,9 +5,9 @@ import asyncio
 import discord
 
 from discord.ext import commands
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
 
 bot = commands.Bot(command_prefix="!", intents=discord.Intents.all())
