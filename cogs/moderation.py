0c596f627d07a0b274458da2525237c6ab0b8017
Dennis <ddhuynh@clemson.edu>
2024-02-24 02:21:14 -0500

    checking for permission to manage messages prior to purging

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -12,18 +12,22 @@ class Moderation(commands.Cog):
     async def purge(self, ctx, amount: int):
         """ Removes a specified number of messages """
 
-        if amount <= 0:
-            await ctx.send("Please provide a valid number of messages to purge.")
-            return
-        
-        # Delete the command message as well
-        amount += 1
+        if ctx.author.guild_permissions.manage_messages:
 
-        try:
-            deleted = await ctx.channel.purge(limit=amount)
-            await ctx.send(f"Deleted {len(deleted)} message(s).", delete_after=5)
-        except Exception as e:
-            await ctx.send(f"An error occurred: {e}")
+            if amount <= 0:
+                await ctx.send("Please provide a valid number of messages to purge.")
+                return
+            
+            # Delete the command message as well
+            amount += 1
+
+            try:
+                deleted = await ctx.channel.purge(limit=amount)
+                await ctx.send(f"Deleted {len(deleted)} message(s) including my message!", delete_after=5)
+            except Exception as e:
+                await ctx.send(f"An error occurred: {e}")
+        else:
+            await ctx.send("You don't have permissions to do that")
         
 async def setup(bot):
     """ Adds cog to bot """
